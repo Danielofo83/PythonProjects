d762780198f8419825cf858dd084e2fe526bc08d
danielofo83 <danielofo83@hotmail.com>
2025-07-13 20:36:33 -0600

    Estos son los primeros cambios

---
+++ Test2.py
@@ -0,0 +1,28 @@
+Test2.py
+
+#estos son los comentarios que dejo de pruebas en Python 
+
+ """
+  # Este es un docstring para una función.
+  # Explica el propósito de la función
+  # y sus parámetros.
+  """
+
+  # comentarios completos
+
+
+  class ClassName(object):
+  	"""docstring for ClassName"""
+  	def __init__(self, arg):
+  		super(ClassName, self).__init__()
+  		self.arg = arg
+  		
+
+ 	class ClassName(object):
+ 		"""docstring for ClassName"""
+ 		def __init__(self, arg):
+ 			super(ClassName, self).__init__()
+ 			self.arg = arg
+ 			
+ 			string nombre = "Luis"
+ 			float numero = numero.string(input())
---
+++ test1.py
@@ -0,0 +1,35 @@
+print("hola mundo")
+
+print("Hello Python world!")
+
+print("Hello Python interpreter!")
+
+message ="esta es una cadena"
+
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+print("Hello, " + full_name.title() + "!")
+
+
+Git Access Token
+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
